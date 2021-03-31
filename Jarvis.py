07731ca92f22c25fac22c8740b64acd2f998d6c3
TechnoFrost27 <60550481+TechnoFrost27@users.noreply.github.com>
2021-03-31 09:04:57 +0530

    Add files via upload

--- Jarvis.py
+++ Jarvis.py
@@ -29,7 +29,9 @@ from PyQt5.QtGui import QMovie
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from jarvisUi import Ui_JarvisGUI
+from PyQt5.uic import loadUiType
+
+from JarvisUi import Ui_JarvisGUI
 import sys
 dictionary = PyDictionary()
 
@@ -988,9 +990,9 @@ class MainThread(QThread):
                         break
 
                 cv2.destroyAllWindows()
-
+'''
 myinstance = MainThread()
-myinstance.TaskExe
+myinstance.TaskExe'''
     
 
 
@@ -1006,27 +1008,30 @@ class Main(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.close)
 
     def startTask(self):
-        self.ui.movie = QtGui.QMovie("../../../Downloads/7LP8.gif")
+        self.ui.movie = QtGui.QMovie("C:\\Users\\user123\\Downloads\\JARVIS GUI\\jarvis-gui1\\Images\7LP8.gif")
         self.ui.label.setMovie(self.ui.movie)
         self.ui.movie.start()
-        self.ui.movie = QtGui.QMovie("../../../Downloads/T8bahf.gif")
-        self.ui.labe2.setMovie(self.ui.movie)
+        self.ui.movie = QtGui.QMovie("C:\\Users\\user123\\Downloads\\JARVIS GUI\\jarvis-gui1\\Images\\T8bahf.gif")
+        self.ui.label_2.setMovie(self.ui.movie)
         self.ui.movie.start()
         timer = QTimer(self)
         timer.timeout.connect(self.showTime)
         timer.start(1000)
         startExecution.start()
+        print("started")
 
     def showTime(self):
         current_time = QTime.currentTime()
         current_date = QDate.currentTime()
         label_time = current_time.toString('hh:mm:ss')
+        print(f"The time is{label_time}")
         label_date = current_date.toString(Qt.ISODate)
+        print(f"The time is{label_date}")
+
         self.ui.textBrowser.setText(label_date)
         self.ui.textBrowser_2.setText(label_time)
 
-
-app = QApplication(sys.argv)
+app=QApplication(sys.argv)
 jarvis = Main()
 jarvis.show()
 exit(app.exec_())
